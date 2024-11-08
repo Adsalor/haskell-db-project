af44af19968d16fde3b81efd8c91dcf733730898
Adsalor <adsalor@gmail.com>
2024-11-08 15:14:24 -0500

    Update Relations.hs
    add some utility functions

--- Data/Relations.hs
+++ Data/Relations.hs
@@ -1,13 +1,27 @@
 module Data.Relations where
 
-import Data.Set
+import Data.Set ( Set, union, isSubsetOf, toAscList )
+import Data.List (intercalate)
 
-newtype Attribute = Attr { str :: String }
+newtype Attribute = Attr { str :: String } deriving (Ord, Eq)
+instance Show Attribute where
+    show = str
 
 type Schema = Set Attribute
 
 data FunctionalDependency = To (Set Attribute) (Set Attribute)
+instance Show FunctionalDependency where
+    show (To l r) = display l ++ "->" ++ display r
+        where
+            display :: Set Attribute -> String
+            display s = intercalate "," (map str $ toAscList s)
 
 type Basis = Set FunctionalDependency
 
 data Relation = Rel Schema Basis
+
+verify :: Relation -> Bool
+verify (Rel schema fds) = all (check schema) fds
+    where
+        check :: Schema -> FunctionalDependency -> Bool
+        check schema (To lhs rhs) = (lhs `union` rhs) `isSubsetOf` schema
