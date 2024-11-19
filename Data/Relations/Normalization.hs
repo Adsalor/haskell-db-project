e6f6cafa36168f0bfaaf1b7369ad644b9cd419e8
Hoppiper26 <130314684+Hoppiper26@users.noreply.github.com>
2024-11-19 15:48:32 -0500

    Normalization functions
    Changed to sets and added is3NF and isBCNF to Normalization

--- Data/Relations/Normalization.hs
+++ Data/Relations/Normalization.hs
@@ -1,15 +1,29 @@
 module Data.Relations.Normalization where 
 
-import Data.Relations ( Relation, Attribute )
+import Data.List (nub)
+import Data.Relations ( Relation (Rel), Attribute, FunctionalDependency (To))
+import Data.Relations.Dependencies ( keysOf )
+import Data.Set qualified as S
 
-primeAttributes :: Relation -> [Attribute]
-primeAttributes = undefined
+is1NF :: Relation -> Bool
+is1NF = const True
+
+primeAttributes :: Relation -> S.Set Attribute
+primeAttributes r = S.unions (keysOf r)
 
 is2NF :: Relation -> Bool
 is2NF = undefined
 
+dependencyIs3NF :: S.Set Attribute -> S.Set (S.Set Attribute) -> FunctionalDependency -> Bool
+dependencyIs3NF pa k (l `To` r) = S.member l k || S.isSubsetOf r pa
+
 is3NF :: Relation -> Bool
-is3NF = undefined
+is3NF r@(Rel s f) = let pa = primeAttributes r;
+                        k = keysOf r 
+                        in all (dependencyIs3NF pa k) f
+
+dependencyIsBCNF :: S.Set (S.Set Attribute) -> FunctionalDependency -> Bool
+dependencyIsBCNF k (l `To` r) = S.member l k
 
 isBCNF :: Relation -> Bool
-isBCNF = undefined
+isBCNF r@(Rel s f) = let k = keysOf r in all (dependencyIsBCNF k) f
